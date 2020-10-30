b715a7f1fddac707c980d2a46115d5afe095f063
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-30 10:58:10 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -10,64 +10,71 @@ window.addEventListener('DOMContentLoaded', (event) => {
     let timerID = setInterval(myTimer, 1000);
 
 
- function myTimer() {
-    counterDisplay.innerText = counter++;
-}
+ 
 
-pauseButton.addEventListener("click", pauseTimer);
-minusButton.addEventListener("click", minusTimer);
-plusButton.addEventListener("click", plusTimer);
-heartButton.addEventListener("click", likeNumber);
-
-
-function pauseTimer() {
-    if (timerSwitch === true) {
-    disableButtons();
-    pause();
-    pauseButton.innerText = "resume";
-    } else {
-        resume();
-        enableButtons();
-        pauseButton.innerText = "pause"
-    }
-    timerSwitch = !timerSwitch;
-}
-
-function minusTimer() {
-    // pause and resume are to remove clumsiness
-    pause();
-    counterDisplay.innerText = counter--;
-    resume();
-}
-
-function plusTimer() {
-    // pause and resume are to remove clumsiness
-    pause();
-    counterDisplay.innerText = counter++;
-    resume();
-}
-
-function disableButtons() {
-    minusButton.removeEventListener("click", minusTimer);
-    plusButton.removeEventListener("click", plusTimer);
-}
-
-function enableButtons() {
+    pauseButton.addEventListener("click", pauseTimer);
     minusButton.addEventListener("click", minusTimer);
     plusButton.addEventListener("click", plusTimer);
-}
+    heartButton.addEventListener("click", likeNumber);
 
-function pause(){
-    clearInterval(timerID);
-}
+    function myTimer() {
+        counterDisplay.innerText = counter++;
+    }
 
-function resume() {
-    timerID = setInterval(myTimer, 1000);
-}
 
-function likeNumber() {
-    console.log(`liked ${counter}`)
-}
+    function pauseTimer() {
+        if (timerSwitch === true) {
+        disableButtons();
+        pause();
+        pauseButton.innerText = "resume";
+        } else {
+            resume();
+            enableButtons();
+            pauseButton.innerText = "pause"
+        }
+        timerSwitch = !timerSwitch;
+    }
+
+    function minusTimer() {
+        // pause and resume are to remove clumsiness
+        pause();
+        counterDisplay.innerText = counter--;
+        resume();
+    }
+
+    function plusTimer() {
+        // pause and resume are to remove clumsiness
+        pause();
+        counterDisplay.innerText = counter++;
+        resume();
+    }
+
+    function disableButtons() {
+        minusButton.removeEventListener("click", minusTimer);
+        plusButton.removeEventListener("click", plusTimer);
+    }
+
+    function enableButtons() {
+        minusButton.addEventListener("click", minusTimer);
+        plusButton.addEventListener("click", plusTimer);
+    }
+
+    function pause(){
+        clearInterval(timerID);
+    }
+
+    function resume() {
+        timerID = setInterval(myTimer, 1000);
+    }
+
+    function likeNumber() {
+        console.log(`liked ${counter}`)
+        let div = document.querySelector("body > div");
+        p = document.createElement("p")
+        p.id=`${counter}`;
+        p.innerText = `x people have liked the number ${p.id}`;
+        div.prepend(p);
+    }
 
 
 
