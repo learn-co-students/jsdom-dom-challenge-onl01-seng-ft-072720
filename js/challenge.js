780826026de18bbc246f6f28d8e96ea8a8cadb96
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-30 10:11:57 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,27 +1,36 @@
 let counter = 0;
+let timerSwitch = true;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    displayTimer();
-    // pauseTimer();
+    const pauseButton = document.querySelector("#pause");
+    let timerID = setInterval(myTimer, 1000);
+
+
+ function myTimer() {
+    document.querySelector("#counter").innerText = counter++;
+}
+
+pauseButton.addEventListener("click", pauseTimer);
+
+function pauseTimer() {
+    if (timerSwitch === true) {
+    console.log("paused");
+    clearInterval(timerID);
+    pauseButton.innerText = "resume";
+    } else {
+        timerID = setInterval(myTimer, 1000);
+        console.log("resume");
+        pauseButton.innerText = "pause"
+    }
+    timerSwitch = !timerSwitch;
+}
+
+
+
+
+
 });
 
 
- displayTimer();
-
- function displayTimer() {   
-    let timerID = setInterval(myTimer, 1000);
-
-    function myTimer() {
-        document.querySelector("#counter").innerText = counter++
-        pauseTimer();
-    }
-
-    function pauseTimer() {
-        pauseButton = document.querySelector("#pause");
-        pauseButton.addEventListener("click", function(event) {
-            clearInterval(timerID);
-        })
-     }
- }
 
  
