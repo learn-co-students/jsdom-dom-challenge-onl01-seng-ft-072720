973c575fae5be877f5c71d06aa3ef8314981cc67
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-30 10:20:41 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -2,15 +2,21 @@ let counter = 0;
 let timerSwitch = true;
 
 window.addEventListener('DOMContentLoaded', (event) => {
+    let counterDisplay = document.querySelector("#counter");
     const pauseButton = document.querySelector("#pause");
+    const minusButton = document.querySelector("#minus");
+    const plusButton = document.querySelector("#plus");
     let timerID = setInterval(myTimer, 1000);
 
 
  function myTimer() {
-    document.querySelector("#counter").innerText = counter++;
+    counterDisplay.innerText = counter++;
 }
 
 pauseButton.addEventListener("click", pauseTimer);
+minusButton.addEventListener("click", minusTimer);
+plusButton.addEventListener("click", plusTimer);
+
 
 function pauseTimer() {
     if (timerSwitch === true) {
@@ -23,6 +29,16 @@ function pauseTimer() {
         pauseButton.innerText = "pause"
     }
     timerSwitch = !timerSwitch;
+}
+
+function minusTimer() {
+    console.log("minus");
+    counterDisplay.innerText = counter--;
+}
+
+function plusTimer() {
+    console.log("plus");
+    counterDisplay.innerText = counter++;
 }
 
 
